4c26d1dd95c243d1d587acaf6e9e8dbfe474c70d
sloniu <Sloniu@PROPC>
2017-03-02 21:07:17 +0100

    Add project files.

---
+++ WowTeamCreator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WowTeamCreator.Startup))]
+namespace WowTeamCreator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
