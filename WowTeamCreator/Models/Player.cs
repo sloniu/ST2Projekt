2bea521271aaf061ee983ef24e209820f9b47607
w53400 <w53400@student.wsiz.rzeszow.pl>
2017-03-03 00:09:38 +0100

    Player model added

---
+++ WowTeamCreator/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WowTeamCreator.Models
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string role { get; set; }
+    }
+}
