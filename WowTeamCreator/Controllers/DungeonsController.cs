ec21c4302a52090d55a6fb4998603aefd8068edc
sloniu <Sloniu@PROPC>
2017-03-02 22:39:30 +0100

    added dungeons and shit

---
+++ WowTeamCreator/Controllers/DungeonsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WowTeamCreator.Controllers
+{
+    public class DungeonsController : Controller
+    {
+        // GET: Dungeons
+        public ActionResult WellOfEternity()
+        {
+            return View();
+        }
+    }
+}
